6c02757514d5052815cf8a03ed13538d9e012155
Francesca Santoriello <f.santoriello@hotmail.com>
2023-05-30 15:11:52 +0200

    valtio Home setup

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,24 @@
+import { motion, AnimatePresence } from 'framer-motion'
+import { useSnapshot } from 'valtio'
+
+import { state } from '../store'
+import { 
+  headContainerAnimation, 
+  headContentAnimation,
+  headTextAnimation,
+  slideAnimation
+} from '../config/motion'
+
+
 const Home = () => {
+  const snap = useSnapshot(state)
+  
   return (
     <div>
-      
+    
     </div>
   )
 }
-
+  
 export default Home
+  
